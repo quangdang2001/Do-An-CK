40f5581303963dc3202a736782c6308641940c75
quangdang2001 <76783582+quangdang2001@users.noreply.github.com>
2020-12-30 22:23:13 +0700

    Update CHI_TIET_SP.cs

--- CHI_TIET_SP.cs
+++ CHI_TIET_SP.cs
@@ -16,13 +16,16 @@ namespace ShopOnline
         public NHANVIEN NhanVienQL { get; set; }
         public NHACUNGCAP NhaCungCap { get; set; }
 
-        public CHI_TIET_SP(SANPHAM SanPham, LOAI_SP MaLoai, string ThuongHieu,string NoiSanXuat, string Mau)
+        public CHI_TIET_SP(SANPHAM SanPham, LOAI_SP MaLoai, string ThuongHieu,string NoiSanXuat, string Mau
+            , NHANVIEN NhanVienQL, NHACUNGCAP NhaCungCap)
         {
             this.SanPham = SanPham;
             this.MaLoai = MaLoai;
             this.ThuongHieu = ThuongHieu;
             this.NoiSanXuat = NoiSanXuat;
             this.Mau = Mau;
+            this.NhanVienQL = NhanVienQL;
+            this.NhaCungCap = NhaCungCap;
         }
     }
 }
