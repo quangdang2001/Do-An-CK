e52c37ff899c5a6087331ea1fe429bc10592531d
quangdang2001 <76783582+quangdang2001@users.noreply.github.com>
2021-01-01 11:19:47 +0700

    Update CHI_TIET_SP.cs

--- CHI_TIET_SP.cs
+++ CHI_TIET_SP.cs
@@ -9,16 +9,14 @@ namespace ShopOnline
     public class CHI_TIET_SP
     {
         public SANPHAM SanPham { get; set; }
-        public LOAI_SP LoaiSP { get; set; }
         public string ThuongHieu { get; set; }
         public string NoiSanXuat { get; set; }
         public string Mau { get; set; }
 
-        public CHI_TIET_SP(SANPHAM SanPham, LOAI_SP LoaiSP, string ThuongHieu,string NoiSanXuat, string Mau
+        public CHI_TIET_SP(SANPHAM SanPham, string ThuongHieu,string NoiSanXuat, string Mau
             )
         {
             this.SanPham = SanPham;
-            this.LoaiSP = LoaiSP;
             this.ThuongHieu = ThuongHieu;
             this.NoiSanXuat = NoiSanXuat;
             this.Mau = Mau;
