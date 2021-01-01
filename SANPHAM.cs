a7aa221a1cdf8cccf7d5952a8c92c1cbffd052d0
quangdang2001 <76783582+quangdang2001@users.noreply.github.com>
2021-01-01 11:19:26 +0700

    Update SANPHAM.cs

--- SANPHAM.cs
+++ SANPHAM.cs
@@ -10,13 +10,16 @@ namespace ShopOnline
         public string TenSP { get; set; }
         public double DonGiaSP { get; set; }
         public NHACUNGCAP NhaCungCap { get; set; }
+        public LOAI_SP LoaiSP { get; set; }
 
-        public SANPHAM(string MaSP, string TenSP, double DonGiaSP, NHACUNGCAP NhaCungCap)
+
+        public SANPHAM(string MaSP, string TenSP, double DonGiaSP, NHACUNGCAP NhaCungCap,LOAI_SP LoaiSP)
         {
             this.MaSP = MaSP;
             this.TenSP = TenSP;
             this.DonGiaSP = DonGiaSP;
             this.NhaCungCap = NhaCungCap;
+            this.LoaiSP = LoaiSP;
         }
     }
 }
